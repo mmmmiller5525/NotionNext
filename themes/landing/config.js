4c1773cbfe8113b183ec1bac5127cf7a7836d16a
mmmmiller5525 <54819394+mmmmiller5525@users.noreply.github.com>
2024-08-18 20:18:10 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -9,14 +9,14 @@ const CONFIG = {
   // 首页大图英雄板块
   LANDING_HERO_TITLE_1: 'Sinopec Technologies',
   LANDING_HERO_P_1: 'Chinese Action Figure ODM',
-  LANDING_HERO_BUTTON_1_TEXT: 'Null1',
-  LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
-  LANDING_HERO_BUTTON_2_TEXT: 'Null2',
-  LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
-  LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
+  //LANDING_HERO_BUTTON_1_TEXT: 'Null1',
+  //LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
+  //LANDING_HERO_BUTTON_2_TEXT: 'Null2',
+  //LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
+  //LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
-  LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
-  LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
+  //LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
+  //LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
 
   // 特性介绍
   LANDING_FEATURES_HEADER_1: 'About Us',
@@ -50,36 +50,38 @@ const CONFIG = {
   LANDING_TESTIMONIALS_HEADER: 'Our Footprint',
   LANDING_TESTIMONIALS_P: 'As an original manufacturer in the industry, we possess strong price competitiveness and have stable raw material suppliers and partner factories. As an international trading company, we have been pursuing a global expansion strategy since the day of our establishment. Currently, we have long-term partnerships with renowned companies, including Walmart, Target, Disney, and Melissa & Doug in the United States; Nikkan, Chica, and Mato in Japan; Bathtime in Europe; and Asura in South Africa.',
 
-  LANDING_TESTIMONIALS_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
-  LANDING_TESTIMONIALS_NICKNAME: 'Ryan_G',
-  LANDING_TESTIMONIALS_ID: 'Ryan`Log 站长',
-  LANDING_TESTIMONIALS_SOCIAL_NAME: '@Gaoran',
-  LANDING_TESTIMONIALS_SOCIAL_URL: 'https://blog.gaoran.xyz/',
-  LANDING_TESTIMONIALS_WORD: '“ 感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ “',
+  //LANDING_TESTIMONIALS_AVATAR: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
+  //LANDING_TESTIMONIALS_NICKNAME: 'Ryan_G',
+  //LANDING_TESTIMONIALS_ID: 'Ryan`Log 站长',
+  //LANDING_TESTIMONIALS_SOCIAL_NAME: '@Gaoran',
+  //LANDING_TESTIMONIALS_SOCIAL_URL: 'https://blog.gaoran.xyz/',
+  //LANDING_TESTIMONIALS_WORD: '“ 感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ “',
 
-  LANDING_POST_REDIRECT_ENABLE: process.env.NEXT_PUBLIC_POST_REDIRECT_ENABLE || false, // 是否开启文章地址重定向 ； 用于迁移旧网站域名
-  LANDING_POST_REDIRECT_URL: process.env.NEXT_PUBLIC_POST_REDIRECT_URL || 'https://blog.tangly1024.com', // 重定向网站地址
+  //LANDING_POST_REDIRECT_ENABLE: process.env.NEXT_PUBLIC_POST_REDIRECT_ENABLE || false, // 是否开启文章地址重定向 ； 用于迁移旧网站域名
+  //LANDING_POST_REDIRECT_URL: process.env.NEXT_PUBLIC_POST_REDIRECT_URL || 'https://blog.tangly1024.com', // 重定向网站地址
 
   LANDING_PRICING_TITLE: 'Contact Us',
   LANDING_PRICING_P: 'If you are interested in business co-operation, feel free to contact！',
+  LANDING_PRICING_P2:'Email: cherryshi2016@126.com',
+  LANDING_PRICING_P3:'Mob: +86 15019431630',
 
-  LANDING_PRICING_1_TITLE: '个人版',
-  LANDING_PRICING_1_PRICE: '免费',
-  LANDING_PRICING_1_CONTENT: '项目源代码,部署教程,不定时技术答疑',
-  LANDING_PRICING_1_BUTTON: '开始体验',
-  LANDING_PRICING_1_URL: 'https://docs.tangly1024.com/about',
+  //LANDING_PRICING_1_TITLE: '个人版',
+  //LANDING_PRICING_1_PRICE: '免费',
+  //LANDING_PRICING_1_CONTENT: '项目源代码,部署教程,不定时技术答疑',
+  //LANDING_PRICING_1_BUTTON: '开始体验',
+  //LANDING_PRICING_1_URL: 'https://docs.tangly1024.com/about',
 
-  LANDING_PRICING_2_TITLE: '捐赠版',
-  LANDING_PRICING_2_PRICE: '$9.9/月',
-  LANDING_PRICING_2_CONTENT: '项目源代码,部署教程,长期技术答疑,代码升级合并,内部社群',
-  LANDING_PRICING_2_BUTTON: '立即购买',
-  LANDING_PRICING_2_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/0adb9153-0799-4f51-91aa-1f06391ea4e0',
+  //LANDING_PRICING_2_TITLE: '捐赠版',
+  //LANDING_PRICING_2_PRICE: '$9.9/月',
+  //LANDING_PRICING_2_CONTENT: '项目源代码,部署教程,长期技术答疑,代码升级合并,内部社群',
+  //LANDING_PRICING_2_BUTTON: '立即购买',
+  //LANDING_PRICING_2_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/0adb9153-0799-4f51-91aa-1f06391ea4e0',
 
-  LANDING_PRICING_3_TITLE: '企业版',
-  LANDING_PRICING_3_PRICE: '$59/月',
-  LANDING_PRICING_3_CONTENT: '项目源代码,部署教程,VIP技术咨询,代码升级合并,内部社群,定制功能开发,SEO优化',
-  LANDING_PRICING_3_BUTTON: '立即购买',
-  LANDING_PRICING_3_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/df924d66-09dc-42a4-a632-a6b0c5cc4f28',
+  //LANDING_PRICING_3_TITLE: '企业版',
+  //LANDING_PRICING_3_PRICE: '$59/月',
+  //LANDING_PRICING_3_CONTENT: '项目源代码,部署教程,VIP技术咨询,代码升级合并,内部社群,定制功能开发,SEO优化',
+  //LANDING_PRICING_3_BUTTON: '立即购买',
+  //LANDING_PRICING_3_URL: 'https://tangly1024.lemonsqueezy.com/checkout/buy/df924d66-09dc-42a4-a632-a6b0c5cc4f28',
 
   LANDING_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_LANDING_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
